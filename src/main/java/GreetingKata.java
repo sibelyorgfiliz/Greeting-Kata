c6f4cbdf49cf05071eafc47ed84031ab4c8b07c3
Syf <sibel.y.filiz@gmail.com>
2021-09-11 22:47:50 +0200

    hello + "uppercase"/"lowercase"

--- src/main/java/GreetingKata.java
+++ src/main/java/GreetingKata.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class GreetingKata {
 public static void main(String [] args) {
     Scanner scan = new Scanner(System.in);
+
     System.out.println("Write your name please: ");
     String name = scan.next();
     System.out.println(greet(name));
@@ -11,9 +12,13 @@ public static void main(String [] args) {
     public static String greet (String name){
         if (name.equals(" ")) {
             return "Hello, my friend.";
-        } else if (name.equals(name.toUpperCase(Locale.ROOT))) {
-            return "HELLO, " + name.toUpperCase(Locale.ROOT);
+        } else if (name.equals(name.toUpperCase())) {
+            return "HELLO, " + name.toUpperCase();
         }
         return "Hello, " + name;
     }
+
+
+
 }
+
