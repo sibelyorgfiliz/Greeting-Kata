67e515ee9258ea027f76bd6450ee5fd4011e2cc3
Syf <sibel.y.filiz@gmail.com>
2021-09-30 12:31:36 +0200

    arraylist- first6Cases-GreetingKata.java is not up to date(have error over "and"). I fixed it in another branch.

--- src/main/java/GreetingKata.java
+++ src/main/java/GreetingKata.java
@@ -1,19 +1,32 @@
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class GreetingKata {
 
     public static void main(String[] args) {
 
-
-        String usersText;
+        List<String> name = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        System.out.print("Write your name please : ");
-        usersText = scan.nextLine();
-        System.out.println(stringnameToArrayname(usersText));
+        String yesNo  = "";
+        boolean yesNoFlag=false;
+        do {
+            System.out.print("Write de name please : ");
+            name.add(scan.nextLine());
+            do{
+                System.out.print("Do you want to write another name? Yes/No : ");
+                yesNo= scan.nextLine();
+                if (!(yesNo.equalsIgnoreCase("no")||yesNo.equalsIgnoreCase("yes"))) {
+                    System.out.println("Please write \"Yes\" or \"No\"");
+                    yesNoFlag=false;
+                } else if (yesNo.equalsIgnoreCase("no") || yesNo.equalsIgnoreCase("yes")){
+                    yesNoFlag = true;
+                }
+            } while (yesNoFlag = false);
+        } while (yesNo.equalsIgnoreCase("yes"));
+
+        //System.out.println(stringnameToArrayname(usersText));
+        System.out.println(greet(name));
     }
-        public static String stringnameToArrayname (String usersText){
+        /*public static String stringnameToArrayname (String usersText){
             String[] name;
             if (usersText.isEmpty()) {
                 //System.out.println("Hello, my friend.");
@@ -26,42 +39,49 @@ public class GreetingKata {
                 return greet(name);
             }
         }
+         */
 
     //"greet" method
-    public static String greet(String[] name) {
+    public static String greet(List<String> name  /*String[] name*/) {
         String greetingNames = "";
         String UPPERgreetingNames = "";
         Boolean flag_Upper = false;
         Boolean flag_Lower = false;
 
-        for (int i = 0; i <= name.length - 1; i++) {
-            if (name[i].equals(name[i].toUpperCase())) {
+        if (name.equals("")) {
+            //System.out.println("Hello, my friend.");
+            return "Hello, my friend.";
+        }
+
+        for (int i = 0; i <= name.size() - 1; i++) {
+            if (name.get(i).equals(name.get(i).toUpperCase())) {
                 flag_Upper = true;
-                if (i == name.length - 1) {
-                    UPPERgreetingNames = UPPERgreetingNames + " " + name[i];
-                } else if (flag_Lower==true) {
-                    UPPERgreetingNames = UPPERgreetingNames  + " "+ name[i];
-                    } else {
-                    UPPERgreetingNames = UPPERgreetingNames + " and " + name[i];
-                    }
+                if (i == name.size() - 1) {
+                    UPPERgreetingNames = UPPERgreetingNames + " " + name.get(i);
+                } else if (flag_Lower == true) {
+                    UPPERgreetingNames = UPPERgreetingNames + " " + name.get(i);
+                } else {
+                    UPPERgreetingNames = UPPERgreetingNames + " and " + name.get(i);
+                }
             } else {
                 flag_Lower = true;
-                if (i == name.length - 1) {
-                    greetingNames = greetingNames +", "+name[i];
-                } else if (flag_Upper==true) {
-                    greetingNames = greetingNames  + " "+name[i];
-                    } else {
-                    greetingNames = greetingNames + " and " + name[i];
-                    }
+                if (i == name.size() - 1) {
+                    greetingNames = greetingNames + ", " + name.get(i);
+                } else if (flag_Upper == true) {
+                    greetingNames = greetingNames + " " + name.get(i);
+                } else {
+                    greetingNames = greetingNames + " and " + name.get(i);
+                }
             }
         }
         //return part
-        if (flag_Upper == true && flag_Lower==true) {
-            return "Hello"+greetingNames + ". AND HELLO"+UPPERgreetingNames+"!";
-        } else if (flag_Upper==false&&flag_Lower==true) {
-            return "Hello" + greetingNames+".";
+        if (flag_Upper == true && flag_Lower == true) {
+            return "Hello" + greetingNames + ". AND HELLO" + UPPERgreetingNames + "!";
+        } else if (flag_Upper == false && flag_Lower == true) {
+            return "Hello" + greetingNames + ".";
         } else {
-            return "HELLO" + UPPERgreetingNames +"!";
+            return "HELLO" + UPPERgreetingNames + "!";
         }
     }
 }
+
--- src/test/java/GreetingKataTest.java
+++ src/test/java/GreetingKataTest.java
@@ -1,43 +1,91 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GreetingKataTest {
 
-    @Test
+   @Test //1
     void shouldHelloName(){
         //given
-        String usersText="Bob";
-
+        //String usersText="Bob";
+       List<String> name = new ArrayList<>();
+       name.add("Bob");
         //when
-        String actual = GreetingKata.stringnameToArrayname(usersText);
+        String actual = GreetingKata.greet(name);
 
         //then
         assertEquals("Hello, Bob.",actual);
     }
 
-    @Test
+    @Test //2
     void HelloNull(){
         //given
-        String usersText="";
-
+        //String usersText="";
+        List<String> name = new ArrayList<>();
+        name.add("");
         //when
-        String actual = GreetingKata.stringnameToArrayname(usersText);
+        String actual = GreetingKata.greet(name);
 
         //then
         assertEquals("Hello, my friend.",actual);
     }
 
-    @Test
+    @Test //3
     void HelloUppercase(){
         //given
-        String usersText="JERRY";
-
+        //String usersText="JERRY";
+        List<String> name = new ArrayList<>();
+        name.add("JERRY");
         //when
-        String actual = GreetingKata.stringnameToArrayname(usersText);
+        String actual = GreetingKata.greet(name);
 
         //then
         assertEquals("HELLO JERRY!",actual);
     }
 
+    @Test //4
+    void Hello2Lowercase(){
+        //given
+        //String usersText="Jill,Jane";
+        List<String> name = new ArrayList<>();
+        name.add("Jill");
+        name.add("Jane");
+        //when
+        String actual = GreetingKata.greet(name);
+        //then
+        assertEquals("Hello, Jill and Jane.",actual);
+    }
+
+    @Test //5
+    void Hello3Lowercase(){
+        //given
+        //String usersText="Amy,Brian,Charlotte";
+        List<String> name = new ArrayList<>();
+        name.add("Amy");
+        name.add("Brian");
+        name.add("Charlotte");
+        //when
+        String actual = GreetingKata.greet(name);
+        //then
+        assertEquals("Hello, Amy, Brian, and Charlotte.",actual);
+    }
+
+    @Test //6
+    void Hello2Lower1Upper(){
+        //given
+        //String usersText="Amy,BRIAN,Charlotte";
+        List<String> name = new ArrayList<>();
+        name.add("Amy");
+        name.add("BRIAN");
+        name.add("Charlotte");
+        //when
+        String actual = GreetingKata.greet(name);
+        //then
+        assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN!",actual);
+    }
 }
